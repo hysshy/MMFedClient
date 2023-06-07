259c83f2b35273b72a48fcd4f3cec798f530be79
hysshy <1197724861@qq.com>
2023-06-07 10:05:20 +0800

    0606

--- server/check_sever.py
+++ server/check_sever.py
@@ -1,15 +1,18 @@
 #向客户端发送文件
 from utils.Log import logger
 import requests
+import threading
+lock = threading.Lock()
 
 def upload_file(filepath, savepath, ip, port):
-    logger.info('向客户端发送文件-' + ip +':'+filepath)
-    with open(filepath, 'rb') as f:
-        response = requests.post('http://{}:{}/download_file'.format(ip, port), files={'file': f}, data={'savepath':savepath})
-    # 检查响应状态码是否为 200
-    if response.status_code == 200:
-        logger.info('File uploaded successfully')
-        return response, True
-    else:
-        logger.info('Failed to upload file')
-        return response, False
+    with lock:
+        logger.info('向客户端发送文件-' + ip +':'+filepath)
+        with open(filepath, 'rb') as f:
+            response = requests.post('http://{}:{}/download_file'.format(ip, port), files={'file': f}, data={'savepath':savepath})
+        # 检查响应状态码是否为 200
+        if response.status_code == 200:
+            logger.info('File uploaded successfully')
+            return response, True
+        else:
+            logger.info('Failed to upload file')
+            return response, False
